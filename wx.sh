524249daa7ca3708bb402d95c4bd79fdfc715daa
liufeng6 <liufeng1026@outlook.com>
2020-05-28 16:40:24 +0800

    shell

--- wx.sh
+++ wx.sh
@@ -7,4 +7,4 @@ pip install PyMySQL==0.9.3
 pip install SQLAlchemy==1.3.17
 pip install wechatsogou==4.5.4
 nohup python -u entry.py > wxarticel.log 2>&1 &
-#curl -O https://raw.githubusercontent.com/atrandys/trojan/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh
+#curl -O https://raw.githubusercontent.com/liufeng1026/rss/master/wx.sh && chmod +x wx.sh && ./wx.sh
